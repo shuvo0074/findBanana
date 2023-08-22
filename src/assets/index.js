631d028767caec9f29aaf9289db9f92eb50b30c2
Tarmim Hoque Shuvo <tarmimhoqueshuvo@gmail.com>
2023-08-23 01:40:55 +0600

    add icon assets

---
+++ src/assets/index.js
@@ -0,0 +1,9 @@
+const HOME = {
+  search: require('./HOME/search.png'),
+  searchInputIcon: require('./HOME/search_icon.png'),
+  emptyList: require('./HOME/emptyList.png'),
+}
+
+export const ASSETS = {
+    ...HOME
+}
