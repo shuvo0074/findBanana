de767319db3d18ec1b26acc70e561544b919d5ed
Tarmim Hoque Shuvo <tarmimhoqueshuvo@gmail.com>
2023-08-23 12:25:18 +0600

    handle undefined selectedItem

--- src/view-controllers/useRankController.js
+++ src/view-controllers/useRankController.js
@@ -9,7 +9,7 @@ const useRankController = () => {
   useEffect(() => {
     if (currentVal.length && userList.length) {
       const selectedItem = userList.find(i => i.name === currentVal)
-      if (selectedItem.rank > 10)
+      if (selectedItem?.rank > 10)
         updateRankList([...userList.slice(0, 9), selectedItem]);
       else
         updateRankList(userList.slice(0, 10));
